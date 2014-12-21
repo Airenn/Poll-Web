7b6e6b5ee87c776931eb736ccc65b4fb478ae7d5
Wachoupa <n6kinnawi>
2014-12-22 00:34:17 +0100

    first commit

---
+++ page-public.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+
+                <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+        <link rel="stylesheet" media="screen" type="text/css" href="css/page-public.php" />
+        <title>Paramètre (Provisoire)</title>
+    </head>
+    <body>
+        <?php
+        session_start();
+        ?>
+        <style> <!---Ceci est degueulasse et je m'en excuse soly-->
+            body{
+                color : <? echo $_SESSION['color'];?>
+            }
+        </style>
+        <h1>Salutt</h1>
+        <p>cc cv toi</p>
+    </body>
+</html>
