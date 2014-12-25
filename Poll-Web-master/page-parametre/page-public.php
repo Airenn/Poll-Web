bfcd6ca42de2c352f75489868c120ab6e410c50f
Airenn <nassim.ahmed.mahmoud@gmail.com>
2014-12-25 19:39:03 +0100

    ajouts des barres progressives

--- Poll-Web-master/page-parametre/page-public.php
+++ Poll-Web-master/page-parametre/page-public.php
@@ -3,6 +3,14 @@
     <head>
         <meta charset="utf-8" />
         <link rel="stylesheet" media="screen" type="text/css" href="css/page-public.php" />
+        <link rel="stylesheet" href="css/bootstrap.css"/>
+        <link rel="stylesheet" href="css/bootstrap.min.css"/>
+
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.easing.min.js"></script>
+        <script src="js/classie.js"></script>
+        <script src="js/cbpAnimatedHeader.js"></script>
         <title>Page public</title>
     </head>
     <body>
@@ -18,10 +26,34 @@
 
             
             <section>
-                <p class="rep">A : Les chiens</p>
-                <p class="rep">B : Les chats</p>
-                <p class="rep">C : Les chevaux</p>
-                <p class="rep">D : Je n'aime pas les animaux</p>
+                <p class="rep">A : Les chiens
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+                            <span class="sr-only">60% Complete</span>
+                        </div>
+                    </div>
+                </p>
+                <p class="rep">B : Les chats
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">60%</div>
+                    </div>
+                </p>
+                <p class="rep">C : Les chevaux
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 45%">
+                        </div>
+                    </div>
+                </p>
+                <p class="rep">D : Je n'aime pas les animaux
+                    <div class="progress">
+                        <div class="progress-bar progress-bar" style="width: 35%">
+                            <span class="sr-only">35% Complete (success)</span>
+                        </div>
+                        <div class="progress-bar progress-bar-warning progress-bar-striped active" style="width: 20%">
+                            <span class="sr-only">20% Complete (warning)</span>
+                        </div>
+                    </div>
+                </p>
             </section>
         
 
