c3edc32f80d37d50b797dccd25b1baae56d4e569
CcCvToa <Jean.Eudes.llas@gmail.Com>
2015-01-08 20:19:42 +0100

    passage du $_GET a questions qui remarche

--- Poll-Web-master/menu.php
+++ Poll-Web-master/menu.php
@@ -40,9 +40,9 @@
             </div>
             <div id="menu">
                 <ul>
-                    <a href='.$accueil.' title="Liste de tous les questionnaires"><li>Questionnaires</li></a>
-                    <a href='.$questions.' title="Afficher/modifier les questions de votre questionnaire"><li>Questions</li></a>
-                    <a href='.$parametre.' title="Paramétrer vos questionnaires"><li>Paramètres</li></a>
+                    <a id="questionnaires" href='.$accueil.' title="Liste de tous les questionnaires"><li>Questionnaires</li></a>
+                    <a id="questions" href='.$questions.' title="Afficher/modifier les questions de votre questionnaire"><li>Questions</li></a>
+                    <a id="parametres" href='.$parametre.' title="Paramétrer vos questionnaires"><li>Paramètres</li></a>
                 </ul>
             </div>
             ';
--- Poll-Web-master/page_accueil/js/passage_get.js
+++ Poll-Web-master/page_accueil/js/passage_get.js
@@ -3,6 +3,7 @@ jQuery(document).ready(function()
 	$('.titre').click(function()
 	{
 		var $id = '../page_questions/questions.php?operation=' + $(this).parent('tr').attr('id');
-		$('.questions').attr({href : $id});
+		alert($id);
+		$('#questions').attr({href : $id});
 	});
 });
