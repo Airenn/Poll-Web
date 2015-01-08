c756c911e5f452c5def70931677e0c25eff126ad
CcCvToa <Jean.Eudes.llas@gmail.Com>
2015-01-08 21:52:41 +0100

    modification : affichage du nom du questionnaire

--- Poll-Web-master/page_accueil/js/passage_get.js
+++ Poll-Web-master/page_accueil/js/passage_get.js
@@ -2,12 +2,12 @@ jQuery(document).ready(function()
 {
 	$('.titre').click(function()
 	{
-		var $selected_id = $(this).parent('tr').attr('id');
+		var $selected_id = $(this).siblings('td').html();
 		var $id = '../page_questions/questions.php?operation=' + $(this).parent('tr').attr('id');
 		//alert($id);
 		//alert($selected_id);
 		$('#questions').attr({href : $id});
 		$('#id_questionnaire_selected').text($selected_id);
-		$('#selected_quesiton').css({width : '33%'});
+		//$('#selected_question').css({width : '33%'});
 	});
 });
