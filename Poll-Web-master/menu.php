9506ea03b681e853cbeb1798c9af942ae1b0afb5
Seeb77 <vasilijevic77@hotmail.com>
2015-01-10 00:12:29 +0100

    Mise à jour du menu

--- Poll-Web-master/menu.php
+++ Poll-Web-master/menu.php
@@ -20,33 +20,31 @@
             $accueil = '"../page_accueil/accueil.php"';
             $questions = '"../page_questions/questions.php"';
             $parametre = '"#" id="current"';
-            $plus='<ul>
-                <li class="liste-entete">
-                    <a class="lien-entete" onclick="maximize_screen();" href="">Afficher la page public</a>
+            $plus='<ul id="liste-entete">
+                <li>
+                    <a class="lien-entete" href="#couleur">Formatage de texte</a>
                 </li>
-                <li class="liste-entete">
+                <li>
                     <a class="lien-entete" href="#arriere-plan">Arrière-Plan</a>
                 </li>
-                <li class="liste-entete">
-                    <a class="lien-entete" href="#couleur">Formatage de texte</a>
+                <li>
+                    <a class="lien-entete" onclick="maximize_screen();" href="">Afficher la page public</a>
                 </li></ul>
             ';
         }
         
         echo'
-            <nav id="menu_bar">
-                <div id="title_logo">
-                    <img src="../poll.png" width="10%" height="10%" id="logo"/>
-                    '.$plus.'
-                </div>
-                <div id="menu">
-                    <ul>
-                        <a id="questionnaires" href='.$accueil.' title="Liste de tous les questionnaires"><li>Questionnaires</li></a>
-                        <a id="questions" href='.$questions.' title="Afficher/modifier les questions de votre questionnaire"><li>Questions</li></a>
-                        <a id="parametres" href='.$parametre.' title="Paramétrer vos questionnaires"><li>Paramètres</li></a>
-                    </ul>
-                </div>
-            </nav>
+            <div id="title_logo">
+                <img src="../poll.png" width="10%" height="10%" id="logo"/>
+                '.$plus.'
+            </div>
+            <div id="menu">
+                <ul>
+                    <a id="questionnaires" href='.$accueil.' title="Liste de tous les questionnaires"><li>Questionnaires</li></a>
+                    <a id="questions" href='.$questions.' title="Afficher/modifier les questions de votre questionnaire"><li>Questions</li></a>
+                    <a id="parametres" href='.$parametre.' title="Paramétrer vos questionnaires"><li>Paramètres</li></a>
+                </ul>
+            </div>
             ';
     }
 
