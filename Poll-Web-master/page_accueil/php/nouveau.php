1d2da832f90301e2c3f24c757b91fd0efe30cf0a
Remi Pierot <rfpierot91@gmail.com>
2015-01-10 12:17:26 +0100

    retouche nouveau/supprimer

--- Poll-Web-master/page_accueil/php/nouveau.php
+++ Poll-Web-master/page_accueil/php/nouveau.php
@@ -1,7 +1,7 @@
 <?php
     require_once('../../php/fonctions.php');
     require_once('../../php/connexion.php');
-    global $db;
+
     $nom_operation = $_GET['nom'];
     $date_operation = $_GET['date'];
     $type_operation='INSERT';
@@ -10,8 +10,7 @@
                     'champs_cibles'=>array('nom','date_prevue','fermee'),
                     'clause_values'=>array('nom'=> $nom_operation,'date_prevue'=>$date_operation,'fermee'=>'0')
                 );
-        execute_sql($type_operation, $table_cible, $args_operation);
-        header("Location: http://localhost/Poll-Web/Poll-Web-master/page_accueil/accueil.php");
-        exit;
+    execute_sql($type_operation, $table_cible, $args_operation);
+
          
 ?>     
--- Poll-Web-master/page_accueil/php/supprimer.php
+++ Poll-Web-master/page_accueil/php/supprimer.php
@@ -1,16 +1,14 @@
 <?php
     require_once('../../php/fonctions.php');
     require_once('../../php/connexion.php');
-    global $db;
+
     $ID_operation = $_GET['ID'];
     $type_operation='DELETE';
     $table_cible='operations';
     $args_operation= array(
-                    'clause_where'=>array('ID'=> $ID_operation)
-                );
-        execute_sql($type_operation, $table_cible, $args_operation);
-        header("Location: http://localhost/Poll-Web/Poll-Web-master/page_accueil/accueil.php");
-        exit;
+                'clause_where'=>array('ID'=> $ID_operation)
+            );
+    execute_sql($type_operation, $table_cible, $args_operation);
          
 ?>      
 
