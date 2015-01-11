0b84f673dc45860a166446ea399a241d5148c517
CcCvToa <Jean.Eudes.llas@gmail.Com>
2015-01-11 17:32:54 +0100

    Correction exporter.php
    TY REMI <3 <3 <3

--- Poll-Web-master/page_accueil/php/exporter.php
+++ Poll-Web-master/page_accueil/php/exporter.php
@@ -5,7 +5,6 @@
 
     $id_operation=$_GET['id'];
     $type_operation='SELECT';
-    //$table_cible='operations';
     $arg = array('clause_where'=>array('id'=>$id_operation));								// Condition de selection des questionnaires ayant pour id $_GET['ID']
 	$args = array('clause_where'=>array('ID_operation'=>$id_operation));					// Condition de selection des questions appartenant au questionnaire voulu
 	
@@ -17,16 +16,14 @@
 	$tab = array();
 	while($question = $que->fetch(PDO::FETCH_ASSOC))										// On récupère sous forme de tableau les questions du questionnaire selectionné
 	{
-		array_push($tab, array($question['ID'],$question['num_question'],$question['texte'],$question['multi_rep'],$question['fermee'],$question['ID_operation']));
-		
+		array_push($tab, array('ID' => $question['ID'],'num_question' => $question['num_question'],'texte' => $question['texte'],'multi_rep' => $question['multi_rep'],'fermee' => $question['fermee'],'ID_operation' => $question['ID_operation']));
 	}
 	
 	$tabl = array();
 	$tubl = array();
-	foreach($tab as /*$key =>*/ $val)															// Si il y a plusieurs questions on parcours le tableau $tab qui contient les questions elle-mêmes récupérées sous forme de tableau
+	foreach($tab as $val)														// Si il y a plusieurs questions on parcours le tableau $tab qui contient les questions elle-mêmes récupérées sous forme de tableau
 	{
-		echo $tab[$val];
-		$argz = array('clause_where'=>array('ID_question'=>$tab[$val]['ID_question']));		// Condition de selection des messages et réponse appartenant a la question selectionnée
+		$argz = array('clause_where'=>array('ID_question'=>$val['ID']));					// Condition de selection des messages et réponse appartenant a la question selectionnée
 																							// Qui peut varier si il y a plusieurs questions
 		$msg = execute_sql($type_operation, "messages", $argz);
 		while($question = $msg->fetch(PDO::FETCH_ASSOC))									// Pour chaque message remplissant la condition précédente
@@ -41,12 +38,10 @@
 		}
 	}
 	
-	//$argr = array('clause_where'=>array('ID_question'=>$tab['ID_question'));
-    //$val = $req->fetch(PDO::FETCH_ASSOC);
-	
 	$nom = htmlentities($ques['nom']).'.poll';										// Nom du fichier (sans lettre avec accent ou caractère spécial) ou sera sauvegardé le questionnaire et totues ses informations relatives
 	echo $nom;
-	$chemin = "../../operations_exportees" . $nom;									// Chemin d'accès depuis accueil.php (qui appelera ce fichier) ou seront sauvegardé les questionnaires
+	$chemin = "../../operations_exportees/" . $nom;									// Chemin d'accès depuis accueil.php (qui appelera ce fichier) ou seront sauvegardé les questionnaires
+	echo $chemin;
 	$quest_export = new questionnaire($ques, $tab, $tabl, $tubl);
     $export_txt = serialize($quest_export);
 	
