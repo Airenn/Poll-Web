dfa7f88ff0897221f13b3efc2fe44ea57899976d
Allaoua NAIT HAMOUCHE <allaoua.nait42@gmail.fr>
2015-01-11 18:43:42 +0100

    maj creation questionnaire

--- Poll-Web-master/page_accueil/php/nouveau.php
+++ Poll-Web-master/page_accueil/php/nouveau.php
@@ -6,11 +6,11 @@
     $date_operation = $_GET['date'];
     $type_operation='INSERT';
     $table_cible='operations';
+    if(isset($nom_operation) && isset($date_operation) && trim($nom_operation)!="" && trim($date_operation)!=""){
     $args_operation= array(
                     'champs_cibles'=>array('nom','date_prevue','fermee'),
                     'clause_values'=>array('nom'=> $nom_operation,'date_prevue'=>$date_operation,'fermee'=>1)
                 );
     execute_sql($type_operation, $table_cible, $args_operation);
-
-         
+    }
 ?>     
